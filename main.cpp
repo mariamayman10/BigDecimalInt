049126a53d2c2551709ad2432b894e6ee7247bfd
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2022-10-10 22:24:00 +0200

    Functions h, k added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+#define ll long long
+string str1, str2;
+int s1, s2, sign1, sign2;
 class BigDecimalInt{
 private:
     string number;
@@ -14,10 +17,10 @@ public:
     //bool operator< (BigDecimalInt anotherDec);
     bool operator> (BigDecimalInt anotherDec);
     bool operator== (BigDecimalInt anotherDec);
-    BigDecimalInt operator= (BigDecimalInt anotherDec);
-    int Size();
+    BigDecimalInt& operator= (BigDecimalInt anotherDec);
+    unsigned ll Size();
     int Sign();
-    //friend ostream& operator << (ostream& out, BigDecimalInt b);
+    friend ostream& operator << (ostream& out, const BigDecimalInt& b);
 };
 BigDecimalInt::BigDecimalInt(){
     number = "0";
@@ -30,37 +33,60 @@ BigDecimalInt::BigDecimalInt(string decStr, int decSz, int decSign){
     sign = decSign;
 }
 bool BigDecimalInt:: operator> (BigDecimalInt anotherDec){
-    if(size > anotherDec.size){
-        return true;
-    }
-    else if(size < anotherDec.size){
-        return false;
-    }
-    else{
-        for (int i = 0; i < size; ++i) {
-            if(number[i] > anotherDec.number[i]){
+    if(number[0] == '-' || anotherDec.number[0] == '-'){
+        if(number[0] == '-' && anotherDec.number[0] != '-'){
+            return false;
+        }
+        else if(number[0] != '-' && anotherDec.number[0] == '-'){
+            return true;
+        }
+        else{
+            if(number.length() - 1 > anotherDec.number.length() - 1){
+                return false;
+            }
+            else if(number.length() - 1 < anotherDec.number.length() - 1){
                 return true;
             }
+            else{
+                for (int i = 0; i < size; ++i) {
+                    if(number[i] > anotherDec.number[i]){
+                        return false;
+                    }
+                }
+            }
+            return false;
         }
+    }
+    else{
+        if(size > anotherDec.size){
+            return true;
+        }
+        else if(size < anotherDec.size){
+            return false;
+        }
+        else{
+            for (int i = 0; i < size; ++i) {
+                if(number[i] > anotherDec.number[i]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+bool BigDecimalInt::operator== (BigDecimalInt anotherDec){
+    if(number == anotherDec.number){
+        return true;
     }
     return false;
 }
-bool BigDecimalInt::operator== (BigDecimalInt anotherDec){
-    int check = 0;
-    if(size != anotherDec.size){
-        return false;
-    }
-    else{
-        for (int i = 0; i < size && check == 0; ++i) {
-            if(number[i] != anotherDec.number[i]){
-                check = 1;
-                return false;
-            }
-        }
-    }
-    return true;
+BigDecimalInt& BigDecimalInt:: operator= (BigDecimalInt anotherDec){
+    number = anotherDec.number;
+    size = anotherDec.size;
+    sign = anotherDec.sign;
+    return *this;
 }
-int BigDecimalInt::Size(){
+unsigned ll BigDecimalInt::Size(){
     if(number[0] == '-'){
         return number.length()-1;
     }
@@ -76,11 +102,15 @@ int BigDecimalInt::Sign(){
         return 1;
     }
 }
-
+ostream& operator << (ostream& out, const BigDecimalInt& b){
+    cout << "Number = ";
+    out << b.number << '\n';
+    return out;
+}
 int main() {
-    string str1, str2;
-    int s1, s2, sign1, sign2;
-    cin >> str1 >> s1 >> str2 >> s2;
+    cin >> str1 >> str2;
+    s1 = str1.length();
+    s2 = str2.length();
     if(str1[0] == '-'){
         sign1 = 0;
     }
@@ -95,4 +125,6 @@ int main() {
     }
     BigDecimalInt n1 = BigDecimalInt(str1, s1, sign1);
     BigDecimalInt n2 = BigDecimalInt(str2, s2, sign2);
+    
 }
+
